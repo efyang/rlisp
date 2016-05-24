86ec39dbeed0ce92ca6369a0f180c414a8e70404
Edward Yang <edward.yang6771@gmail.com>
2016-05-24 08:47:37 -0500

    Rewrite function eval

--- src/eval.rs
+++ src/eval.rs
@@ -68,62 +68,33 @@ impl Eval for Expr {
     fn eval(&self, env: &mut Env) -> Result<Option<Object>, String> {
         match *self {
             Expr::Exprs(ref exprs) => {
-                let (head, tail): (&Expr, &[Expr]) = exprs.split_first().unwrap();
-                if let Expr::Expr(Object::Symbol(ref function_name)) = *head {
+                let (orig_head, tail): (&Expr, &[Expr]) = exprs.split_first().unwrap();
+                let head;
+                if let Expr::Exprs(_) = *orig_head {
+                    head = Expr::Expr(try!((*orig_head).eval(env)).unwrap());
+                } else {
+                    head = (*orig_head).clone();
+                }
+                if let Expr::Expr(Object::Symbol(ref function_name)) = head {
                     let args = tail.to_vec();
                     if function_name == "define" {
                         let (first, rest) = args.split_first().unwrap();
                         match *first {
                             Expr::Expr(Object::Symbol(ref var)) => {
-                                //define a variable
-                                match args.last().unwrap().clone().eval(env) {
-                                    Ok(Some(value)) => {
-                                        env.add_variable(var.clone(), value);
-                                        return Ok(None);
-                                    },
-                                    Ok(None) => return Err("Cannot set variable to nonetype".to_string()),
-                                    Err(e) => return Err(e),
-                                };
+                                define_variable(var, rest, env)
                             },
                             Expr::Expr(ref tried_ident) => {
-                                return Err(format!("Invalid variable identifier \"{:?}\"", tried_ident));
+                                Err(format!("Invalid variable identifier \"{:?}\"", tried_ident))
                             },
                             Expr::Exprs(ref fndef) => {
-                                //define a function
-                                //TODO
-                                if args.len() < 2 {
-                                    return Err(format!("Function body of function {:?} too short", fndef[0]));
-                                } else {
-                                    if let Expr::Expr(Object::Symbol(ref fn_name)) = fndef[0] {
-                                        let fnargs = &fndef[1..fndef.len()];
-                                        let body = rest;
-                                        let function = try!(Function::from_exprs(fnargs, body));
-                                        env.add_variable(fn_name.to_string(), Object::Function(function));
-                                        return Ok(None);
-                                    } else {
-                                        return Err(format!("Invalid function identifier {:?}", fndef[0]));
-                                    }
-                                }
+                                define_function(fndef, rest, env)
                             },
                         }
-                        Ok(None)
+                    } else {
+                        eval_function_named(function_name, &args, env)
                     }
-                    /*else if function_name == &"quote".to_string() {
-                      Ok(Some())
-                      }*/
-                    else {
-                        let mut evaluated: Vec<Object> = Vec::new();
-                        for expr in args.iter() {
-                            let evalresult = expr.eval(env);
-                            match evalresult {
-                                Ok(Some(Object::Exit(_))) => return evalresult,
-                                Ok(Some(r)) => evaluated.push(r),
-                                Ok(None) => {},
-                                Err(_) => return evalresult,
-                            }
-                        }
-                        eval_function(function_name, evaluated, env)
-                    }
+                } else if let Expr::Expr(Object::Function(ref function)) = head {
+                    eval_function((*function).clone(), tail, env)
                 } else {
                     Err(format!("Invalid function name {:?}", head))
                 }
@@ -144,48 +115,88 @@ impl Eval for Expr {
     }
 }
 
-fn eval_function(function_name: &String, args: Vec<Object>, env: &mut Env) -> Result<Option<Object>, String> {
+fn define_variable(var: &str, args: &[Expr], env: &mut Env) -> Result<Option<Object>, String> {
+    match args.last().unwrap().clone().eval(env) {
+        Ok(Some(value)) => {
+            env.add_variable(var.to_string(), value);
+            return Ok(None);
+        },
+        Ok(None) => return Err("Cannot set variable to nonetype".to_string()),
+        Err(e) => return Err(e),
+    };
+}
+
+fn define_function(declaration: &[Expr], args: &[Expr], env: &mut Env) -> Result<Option<Object>, String> {
+    if args.len() > 1 {
+        return Err(format!("Function body of function {:?} too short", declaration[0]));
+    } else {
+        if let Expr::Expr(Object::Symbol(ref fn_name)) = declaration[0] {
+            let fnargs = &declaration[1..declaration.len()];
+            let body = args;
+            let function = try!(Function::from_exprs(fnargs, body));
+            env.add_variable(fn_name.to_string(), Object::Function(function));
+            return Ok(None);
+        } else {
+            return Err(format!("Invalid function identifier {:?}", declaration[0]));
+        }
+    }
+}
+
+fn eval_function_named(function_name: &str, args: &[Expr], env: &mut Env) -> Result<Option<Object>, String> {
     let function = match_first_function(function_name, env.variables());
     if function.is_ok() {
-        match *function.ok().unwrap().procedure {
-            LispFn::Builtin(ref innerfn) => {
-                let evaluated = (innerfn.inner())(args, env);
-                match evaluated {
-                    Ok(Some(r)) => Ok(Some(r)),
-                    Ok(None) => Ok(None),
-                    Err(e) => Err(e)
-                }
+        eval_function(function.ok().unwrap(), args, env)
+    } else {
+        Err(function.err().unwrap())
+    }
+}
+
+fn eval_function(function: Function, args: &[Expr], env: &mut Env) -> Result<Option<Object>, String> {
+    let mut evaled_args: Vec<Object> = Vec::new();
+    for expr in args.iter() {
+        let evalresult = expr.eval(env);
+        match evalresult {
+            Ok(Some(Object::Exit(_))) => return evalresult,
+            Ok(Some(r)) => evaled_args.push(r),
+            Ok(None) => {},
+            Err(_) => return evalresult,
+        }
+    }
+    match *function.procedure {
+        LispFn::Builtin(ref innerfn) => {
+            let evaluated = (innerfn.inner())(evaled_args, env);
+            match evaluated {
+                Ok(Some(r)) => Ok(Some(r)),
+                Ok(None) => Ok(None),
+                Err(e) => Err(e)
             }
-            LispFn::UserDef(ref vars, ref body) => {
-                if args.len() != vars.len() {
-                    Err(format!("Function {:?} run with {} args; should be run with {} args", function_name, args.len(), vars.len()))
+        }
+        LispFn::UserDef(ref vars, ref body) => {
+            if evaled_args.len() != vars.len() {
+                Err(format!("Function {:?} run with {} args; should be run with {} args", function, evaled_args.len(), vars.len()))
+            } else {
+                let mut var_mappings = HashMap::new();
+                for (var, var_mapping) in vars.iter().zip(evaled_args.iter()) {
+                    var_mappings.insert(var, var_mapping);
+                }
+                let newbody = body
+                    .iter()
+                    .map(|ref expr| expr.replace_all(&var_mappings))
+                    .collect::<Vec<_>>();
+                if newbody.len() > 1 {
+                    let (final_expr, leading_exprs) = body.split_last().unwrap();
+                    for expr in leading_exprs {
+                        try!(expr.replace_all(&var_mappings).eval(env));
+                    }
+                    final_expr.replace_all(&var_mappings).eval(env)
                 } else {
-                    let mut var_mappings = HashMap::new();
-                    // arg -> arg_mapping
-                    for (var, var_mapping) in vars.iter().zip(args.iter()) {
-                        var_mappings.insert(var, var_mapping);
-                    }
-                    let newbody = body
-                        .iter()
-                        .map(|ref expr| expr.replace_all(&var_mappings))
-                        .collect::<Vec<_>>();
-                    if newbody.len() > 1 {
-                        let (final_expr, leading_exprs) = body.split_last().unwrap();
-                        for expr in leading_exprs {
-                            try!(expr.replace_all(&var_mappings).eval(env));
-                        }
-                        final_expr.replace_all(&var_mappings).eval(env)
-                    } else {
-                        newbody.first()
-                            .unwrap()
-                            .replace_all(&var_mappings)
-                            .eval(env)
-                    }
+                    newbody.first()
+                        .unwrap()
+                        .replace_all(&var_mappings)
+                        .eval(env)
                 }
             }
         }
-    } else {
-        Err(function.err().unwrap())
     }
 }
 
